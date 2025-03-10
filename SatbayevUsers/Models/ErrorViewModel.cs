a7e7793e26b93f07cff6208fb5c9a80a2a309c2b
Almas Yoko_office <Almas almas@yoko.space>
2025-03-10 16:14:37 +0500

    Add project files.

---
+++ SatbayevUsers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SatbayevUsers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
