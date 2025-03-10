0b2a270a4cd78de5b44f0209b4902629f81c4bb5
Almas Yoko_office <Almas almas@yoko.space>
2025-03-10 20:53:21 +0500

    Добавлены методы для Удаления, редактирования. Настроена валидация для нагрузки

--- SatbayevUsers/Controllers/UsersController.cs
+++ SatbayevUsers/Controllers/UsersController.cs
@@ -20,28 +20,117 @@ namespace SatbayevUsers.Controllers
             return View(user);
         }
 
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-        //[HttpPost]
+        [HttpPost]
         public async Task<IActionResult> Create([Bind("Id,Name,Email,DateOfBirth,IIN")] User user)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            } else
-            {
+
                 Console.WriteLine(user.Name);
                 Console.WriteLine(user.Email);
                 Console.WriteLine(user.DateOfBirth);
                 Console.WriteLine(user.IIN);
                 Console.WriteLine("Invalid model state");
+
+                return View(user);
             }
-            return View(user);
+
+            var userExists = await _context.Users.AnyAsync(x => x.IIN == user.IIN || x.Email == user.Email);
+            
+            if (userExists)
+            {
+                ModelState.AddModelError("IIN", "User with this IIN or Email already exists");
+                return View(user);
+            }
+
+            var userDays = (DateTime.Now - user.DateOfBirth).TotalDays;
+
+            if (userDays < 6570)
+            {
+                ModelState.AddModelError("DateOfBirth", "User must be at least 18 years old");
+                return View(user);
+            }
+
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _context.Users.FindAsync(id);
+            return View(item);
+        }
+
+        //[HttpPut("/users/edit/{id}")]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,DateOfBirth,IIN")] User user)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Update(user);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    } else
+        //    {
+        //        Console.WriteLine(user.Name);
+        //        Console.WriteLine(user.Email);
+        //        Console.WriteLine(user.DateOfBirth);
+        //        Console.WriteLine(user.IIN);
+        //        Console.WriteLine("Invalid model state");
+        //    }
+        //        return View(user);
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,DateOfBirth,IIN")] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            var userExists = await _context.Users.AnyAsync(x => (x.IIN == user.IIN || x.Email == user.Email) && x.Id != user.Id);
+
+            if (userExists)
+            {
+                ModelState.AddModelError("IIN", "User with this IIN or Email already exists");
+                return View(user);
+            }
+
+            var userDays = (DateTime.Now - user.DateOfBirth).TotalDays;
+
+            if (userDays < 6570)
+            {
+                ModelState.AddModelError("DateOfBirth", "User must be at least 18 years old");
+                return View(user);
+            }
+
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var itemToDelete = await _context.Users.FindAsync(id);
+            if (itemToDelete != null)
+            {
+                _context.Users.Remove(itemToDelete);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- SatbayevUsers/Models/User.cs
+++ SatbayevUsers/Models/User.cs
@@ -1,11 +1,22 @@
-﻿namespace SatbayevUsers.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SatbayevUsers.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; } = null!;
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
         public string Email { get; set; } = null!;
+        [Required]
         public DateTime DateOfBirth { get; set; }
-        public string IIN { get; set; }
+        [Required]
+        [StringLength(12)]
+        [RegularExpression(@"^\d{12}$")]
+        public string IIN { get; set; } = null!;
     }
 }
